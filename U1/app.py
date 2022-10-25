b5c46e57573724c4be46d0c37891ec51bb6a8ead
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-10-25 14:23:46 +0200

    create load & show image function

--- U1/app.py
+++ U1/app.py
@@ -2,10 +2,17 @@
 import os
 import cv2
 
-currentPath = os.path.abspath(os.getcwd())
-print("Path:", currentPath)
+print("Path:", os.path.abspath(os.getcwd()))
 
-# img = cv2.imread('Lena.bmp', cv2.IMREAD_GRAYSCALE)
-# print('Image loaded')
-# cv2.namedWindow('main')
-# cv2.imshow('main', img)
+
+def loadShowImage():
+    img = cv2.imread('./Lena.bmp', cv2.IMREAD_GRAYSCALE)
+    print('Image loaded')
+    cv2.namedWindow('main')
+    cv2.imshow('main', img)
+    print('Waiting for user input...')
+    cv2.waitKey(0)
+    print('Closing all windows')
+    cv2.destroyAllWindows()
+
+loadShowImage()
