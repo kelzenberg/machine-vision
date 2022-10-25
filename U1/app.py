e78f96e6d61ee9137aa98345df495ad6828d8a9c
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-10-25 21:10:50 +0200

    draw colored rectangles on gray image

--- U1/app.py
+++ U1/app.py
@@ -1,27 +1,58 @@
 """U1 app"""
 import cv2
 
-imgWindowName = "main"
+mainImage = cv2.imread('./Lena.bmp')
+grayImage = cv2.cvtColor(mainImage, cv2.COLOR_RGB2GRAY)
+print('[DEBUG](main) Image loaded.')
+
+mainWindowName = 'main'
+cv2.namedWindow(mainWindowName)
+
+rectPoints = [None, None]
+color = (0, 0, 255)  # Red in BGR
 
 
-def showImage(path: str = './Lena.bmp'):
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    print('Image loaded.')
-    cv2.namedWindow(imgWindowName)
-    print('Show image...')
-    cv2.imshow(imgWindowName, img)
+def showImage(image, windowName=mainWindowName):
+    print('[DEBUG](showImage) Show image in window:', windowName)
+    cv2.imshow(windowName, image)
 
 
-def onMouse(event: int, x: int, y: int, flags: int, userdata):
-    if event == 1:
-        print('onMouse @click:', x, y, flags, userdata)
+def setRectanglePoints(x: int, y: int, slot: int):
+    if slot == 0:
+        rectPoints[0] = (x, y)
+        return
+    if slot == 1:
+        rectPoints[1] = (x, y)
         return
 
 
-showImage()
-cv2.setMouseCallback(imgWindowName, onMouse)
+def drawRectOnImage(image):
+    point1 = rectPoints[0]
+    point2 = rectPoints[1]
 
-print('Press Q to exit...')
+    if point1 != None and point2 != None:
+        return cv2.rectangle(image, point1, point2, color, 2)
+
+
+def onMouse(event: int, x: int, y: int, flags: int, userdata=None):
+    if event == 1:  # left-mouse-down
+        print('[DEBUG](onMouse) @Left-Click-DOWN:', x, y, flags)
+        setRectanglePoints(x, y, slot=0)
+        return
+
+    if event == 4:  # left-mouse-up
+        print('[DEBUG](onMouse) @Left-Click-UP:', x, y, flags)
+        setRectanglePoints(x, y, slot=1)
+
+        BGRGrayImage = cv2.cvtColor(grayImage, cv2.COLOR_GRAY2BGR)
+        showImage(drawRectOnImage(BGRGrayImage))
+        return
+
+
+showImage(grayImage)
+cv2.setMouseCallback(mainWindowName, onMouse)
+
+print('[DEBUG](main) Press Q to exit...')
 keyCode = cv2.waitKeyEx(0)
-print('Closing all windows.', keyCode)
+print('[DEBUG](main) Closing all windows.', keyCode)
 cv2.destroyAllWindows()
