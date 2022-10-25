ec87c5f3c69923de0c95123ef38ce56ff932e5ae
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-10-25 12:24:33 +0200

    add U1 project

---
+++ U1/app.py
@@ -0,0 +1,11 @@
+"""U1 app"""
+import os
+import cv2
+
+currentPath = os.path.abspath(os.getcwd())
+print("Path:", currentPath)
+
+# img = cv2.imread('Lena.bmp', cv2.IMREAD_GRAYSCALE)
+# print('Image loaded')
+# cv2.namedWindow('main')
+# cv2.imshow('main', img)
