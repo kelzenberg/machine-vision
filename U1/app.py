737d30ad90ba510327f08fc81a4225c700166b72
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-02 22:16:55 +0100

    unify point-checking, add plain zoom LUT & comments

--- U1/app.py
+++ U1/app.py
@@ -6,34 +6,37 @@ grayImage = cv2.cvtColor(cv2.cvtColor(
     mainImage, cv2.COLOR_RGB2GRAY), cv2.COLOR_GRAY2BGR)
 print('[DEBUG](main) Image loaded.')
 
-mainWindowName = 'main'
-zoomWindowName = 'zoom'
+mainWindowName = 'Main'
+zoomWindowName = 'Zoom & LUT'
 cv2.namedWindow(mainWindowName, cv2.WINDOW_KEEPRATIO)
 cv2.moveWindow(mainWindowName, 100, 100)
 cv2.setWindowProperty(mainWindowName, cv2.WND_PROP_TOPMOST, 1)
 
-rectPoints = [None, None]
-sortedRectPoints = [None, None]
+rectanglePoints = [None, None]
+sortedRectanglePoints = [None, None]
 redColor = (0, 0, 255)  # Red in BGR
 zoomValue = 0
 lutValue = 0
 
+"""
+Utility functions
+"""
 
-def showImage(image, windowName):
+
+def showImage(windowName, image):
     print('[DEBUG](showImage) Show image in window:', windowName)
     cv2.imshow(windowName, image)
 
 
-def setRectanglePoints(x: int, y: int, slot: int):
-    if slot == 0:
-        rectPoints[0] = (x, y)
-        return
-    if slot == 1:
-        rectPoints[1] = (x, y)
-        return
+def convertToGrayImage(image):
+    return cv2.cvtColor(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY), cv2.COLOR_GRAY2BGR)
 
 
-def sortRectanglePoints(points):
+def writeText(image, text):
+    return cv2.putText(image, text, (10, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, redColor, 1, cv2.LINE_AA)
+
+
+def sortTwoPoints(points):
     [pointA, pointB] = points
     x1 = pointA[0] if pointA[0] < pointB[0] else pointB[0]
     x2 = pointB[0] if pointB[0] > pointA[0] else pointA[0]
@@ -42,64 +45,108 @@ def sortRectanglePoints(points):
     return [(x1, y1), (x2, y2)]
 
 
+def isValidPoints(points):
+    if points[0] == None or points[1] == None:
+        # At least one Point is missing
+        return False
+    elif points[1][0]-points[0][0] <= 0 or points[1][1]-points[0][1] <= 0:
+        # From- and To-Points are closer than 1 Pixel
+        return False
+    else:
+        # Points are valid
+        return True
+
+
+"""
+Rectangle functions
+"""
+
+
+def setRectanglePoints(x: int, y: int, slot: int):
+    if slot == 0:
+        rectanglePoints[0] = (x, y)
+        return
+    if slot == 1:
+        rectanglePoints[1] = (x, y)
+        return
+
+
 def drawRectangle(points):
     print(f'[DEBUG](drawRectangle) with Points {points}')
     [fromPoint, toPoint] = points
-    showImage(cv2.rectangle(grayImage.copy(),
-                            fromPoint, toPoint, redColor, 2), mainWindowName)
+    showImage(mainWindowName, cv2.rectangle(
+        grayImage.copy(), fromPoint, toPoint, redColor, 2))
 
 
-def initZoomWindow():
+"""
+Zoom & LUT functions
+"""
+
+
+def initZoomLUTWindow():
     cv2.destroyWindow(zoomWindowName)
     cv2.namedWindow(zoomWindowName, cv2.WINDOW_AUTOSIZE)
     cv2.moveWindow(zoomWindowName, 666, 100)
 
 
-def updateZoomWindow(points):
+def updateZoomLUTWindow(points):
     print(f'[DEBUG](updateZoomWindow) with Points {points}')
     [(x1, y1), (x2, y2)] = points
     croppedImage = mainImage[y1:y2, x1:x2]
 
-    zoomedImage = cv2.resize(
-        croppedImage, None, fx=zoomValue + 1, fy=zoomValue + 1, interpolation=cv2.INTER_CUBIC)
-
-    displayedImage = None
+    displayedImage = cv2.resize(croppedImage, None, fx=zoomValue + 1,
+                                fy=zoomValue + 1, interpolation=cv2.INTER_CUBIC)
 
     match lutValue:
+        case 0:
+            pass
         case 13:
-            invertedImage = cv2.bitwise_not(zoomedImage)
-            convertedImage = cv2.cvtColor(cv2.cvtColor(
-                invertedImage, cv2.COLOR_RGB2GRAY), cv2.COLOR_GRAY2BGR)
-            displayedImage = cv2.putText(
-                convertedImage, f'Zoom: {zoomValue + 1}', (10, 20), cv2.FONT_HERSHEY_COMPLEX, 0.5, redColor, 1, cv2.LINE_AA)
+            convertedImage = convertToGrayImage(displayedImage)
+            displayedImage = writeText(
+                convertedImage, f'Zoom: {zoomValue + 1}')
+            pass
+        case 14:
+            invertedImage = cv2.bitwise_not(displayedImage)
+            convertedImage = convertToGrayImage(invertedImage)
+            displayedImage = writeText(
+                convertedImage, f'Zoom: {zoomValue + 1}')
+            pass
         case _:
-            displayedImage = zoomedImage
+            if 0 <= lutValue - 1 <= 11:
+                displayedImage = cv2.applyColorMap(
+                    displayedImage, lutValue - 1)
+            pass
 
-    showImage(displayedImage, zoomWindowName)
+    showImage(zoomWindowName, displayedImage)
+
+
+"""
+ON-Event Handler
+"""
 
 
 def onMouse(event: int, x: int, y: int, flags: int, userdata=None):
     if event == 1:  # left-mouse-down
         print('[DEBUG](onMouse) @Left-Click-DOWN:', x, y, flags)
         setRectanglePoints(x, y, slot=0)
-        initZoomWindow()
+        initZoomLUTWindow()
         return
 
     if event == 0 and flags == 1:  # mouse moved & left-mouse down
         print('[DEBUG](onMouse) @Mouse-Moved:', x, y, flags)
         setRectanglePoints(x, y, slot=1)
 
-        if rectPoints[0] == None or rectPoints[1] == None:
+        global rectanglePoints
+        if rectanglePoints[0] == None or rectanglePoints[1] == None:
             return
 
-        global sortedRectPoints
-        sortedRectPoints = sortRectanglePoints(rectPoints)
-
-        if sortedRectPoints[1][0]-sortedRectPoints[0][0] <= 0 or sortedRectPoints[1][1]-sortedRectPoints[0][1] <= 0:
+        global sortedRectanglePoints
+        sortedRectanglePoints = sortTwoPoints(rectanglePoints)
+        if not isValidPoints(sortedRectanglePoints):
             return
 
-        drawRectangle(sortedRectPoints)
-        updateZoomWindow(sortedRectPoints)
+        drawRectangle(sortedRectanglePoints)
+        updateZoomLUTWindow(sortedRectanglePoints)
         return
 
 
@@ -111,10 +158,11 @@ def onChangeZoom(value):
     print(f'[DEBUG](onChangeZoom) @Zoom-Changed: {zoomValue} to {value}')
     zoomValue = value
 
-    if sortedRectPoints[0] == None or sortedRectPoints[1] == None:
+    global sortedRectanglePoints
+    if not isValidPoints(sortedRectanglePoints):
         return
 
-    updateZoomWindow(sortedRectPoints)
+    updateZoomLUTWindow(sortedRectanglePoints)
 
 
 def onChangeLUT(value):
@@ -125,18 +173,24 @@ def onChangeLUT(value):
     print(f'[DEBUG](onChangeLUT) @LUT-Changed: {lutValue} to {value}')
     lutValue = value
 
-    if sortedRectPoints[0] == None or sortedRectPoints[1] == None:
+    global sortedRectanglePoints
+    if not isValidPoints(sortedRectanglePoints):
         return
 
-    updateZoomWindow(sortedRectPoints)
+    updateZoomLUTWindow(sortedRectanglePoints)
 
+
+"""
+Main function
+"""
 
 cv2.setMouseCallback(mainWindowName, onMouse)
 cv2.createTrackbar('Zoom:', mainWindowName, zoomValue, 3, onChangeZoom)
-cv2.createTrackbar('LUT:', mainWindowName, lutValue, 13, onChangeLUT)
-# Trackbar creates errors on CV2 v4.6.x release -> fix is in v5.x, see https://github.com/opencv/opencv/issues/22561#issuecomment-1257164504
+cv2.createTrackbar('LUT:', mainWindowName, lutValue, 14, onChangeLUT)
+# Trackbar creates errors on OpenCV v4.6.x release on MacOS -> fixed in v5.x
+# see https://github.com/opencv/opencv/issues/22561#issuecomment-1257164504
 
-showImage(grayImage, mainWindowName)
+showImage(mainWindowName, grayImage)
 
 print('[DEBUG](main) Press ESC to exit...')
 while cv2.waitKey(0) != 27:
