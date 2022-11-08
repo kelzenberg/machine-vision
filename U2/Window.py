99034434a002fdbfbe84b2da54883844cc06e2d4
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-08 14:49:33 +0100

    add scaling of images to Window class

--- U2/Window.py
+++ U2/Window.py
@@ -2,9 +2,10 @@ import cv2
 
 
 class Window:
-    def __init__(self, name, image, offset=(100, 100)):
+    def __init__(self, name, image, scale=0.33, offset=(0, -20)):
         self.name = name
-        self.image = image
+        self.image = cv2.resize(image, None, fx=scale,
+                                fy=scale, interpolation=cv2.INTER_AREA)
         cv2.namedWindow(name, cv2.WINDOW_KEEPRATIO)
         cv2.moveWindow(name, offset[0], offset[1])
         cv2.setWindowProperty(name, cv2.WND_PROP_TOPMOST, 1)
