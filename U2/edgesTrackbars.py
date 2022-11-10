f637f7c87da9928063cb5bd79bbd37687a9166fb
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-10 18:44:42 +0100

    merge edgesTrackbars with edgesWindow

--- U2/edgesTrackbars.py
+++
@@ -1,2 +0,0 @@
-def noopFunc(arg):
-    print('(noopFunc)', arg)
--- U2/edgesWindow.py
+++ U2/edgesWindow.py
@@ -1,10 +1,22 @@
 import imageWindow
 import filterWindow
 from Window import Window
-from edgesTrackbars import noopFunc as noop3
+
+"""
+Trackbar Functions
+"""
+
+
+def noopFunc(arg):
+    print('(noopFunc)', arg)
+
+
+"""
+Window Functions
+"""
 
 window = Window('Edges', filterWindow.window.image, scale=0.4,
-                     offset=(0, round(imageWindow.window.preview.shape[1]/1.25)))
-window.addTrackbar('Threshold: ', 0, 255, noop3)
-window.addTrackbar('Display Image: ', 0, 5, noop3)
+                offset=(0, round(imageWindow.window.preview.shape[1]/1.25)))
+window.addTrackbar('Threshold: ', 0, 255, noopFunc)
+window.addTrackbar('Display Image: ', 0, 5, noopFunc)
 window.show()
