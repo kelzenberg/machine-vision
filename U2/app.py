954c4ea563f1857550c7bd4cc05bf1188444f1c4
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-10 18:14:34 +0100

    add sigma setting for gauss

--- U2/app.py
+++ U2/app.py
@@ -4,7 +4,7 @@ U2 app
 import cv2
 from Window import Window
 from imageTrackbars import noopFunc as noop1
-from filterTrackbars import noopFunc as noop2
+from filterTrackbars import noopFunc as noop2, sigma
 from edgesTrackbars import noopFunc as noop3
 from utils import convertToGrayBGR
 
@@ -28,7 +28,7 @@ imageWindow.show()
 
 filterWindow = Window('Filter', imageWindow.image,
                       offset=(round(imageWindow.preview.shape[0]*1.8), 0))
-filterWindow.addTrackbar('Sigma: ', 0, 255, noop2)
+filterWindow.addTrackbar('Sigma: ', 0, 5, sigma)
 filterWindow.addTrackbar('Size: ', 0, 4, noop2)
 filterWindow.show()
 
--- U2/filterTrackbars.py
+++ U2/filterTrackbars.py
@@ -1,2 +1,7 @@
 def noopFunc(arg):
     print('(noopFunc)', arg)
+
+
+def sigma(value):
+    newValue = (value + 1) / 10
+    print('(sigma)', newValue)
