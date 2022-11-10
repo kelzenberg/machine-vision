c72030dd056891f8ee1ea6600016274665ddcc7e
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-10 18:41:54 +0100

    merge imageTrackbars with imageWindow

--- U2/imageWindow.py
+++ U2/imageWindow.py
@@ -1,7 +1,6 @@
 import cv2
 from utils import convertToGrayBGR
 from Window import Window
-from imageTrackbars import noopFunc as noop1, filterOnChange
 
 """
 Load Image
@@ -11,7 +10,31 @@ mainImage = cv2.imread('./Stop.jpg')
 grayImage = convertToGrayBGR(mainImage)
 print('(main) Image loaded.')
 
+"""
+Trackbar Functions
+"""
+
+filterValue = 0
+
+
+def noopFunc(arg):
+    print('(noopFunc)', arg)
+
+
+def filterOnChange(value):
+    global filterValue
+    if filterValue == value:
+        return
+
+    filterValue = value
+    print('(filterOnChange)', filterValue)
+
+
+"""
+Window Functions
+"""
+
 window = Window('Image', grayImage)
-window.addTrackbar('Operation: ', 0, 3, noop1)
+window.addTrackbar('Operation: ', 0, 3, noopFunc)
 window.addTrackbar('Filter: ', 0, 2, filterOnChange)
 window.show()
