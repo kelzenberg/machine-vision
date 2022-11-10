0bb138a229ee5f37c49a7b1f54083fe5d6b6cd75
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-10 18:43:48 +0100

    merge filterTrackbars with filterWindow

--- U2/filterWindow.py
+++ U2/filterWindow.py
@@ -1,10 +1,35 @@
-import cv2
 from Window import Window
 import imageWindow
-from filterTrackbars import noopFunc as noop2, sigmaOnChange
+
+"""
+Trackbar Functions
+"""
+
+sigmaValue = 0.1
+
+
+def noopFunc(arg):
+    print('(noopFunc)', arg)
+
+
+def sigmaOnChange(value):
+    newValue = (value + 1) / 10
+
+    global sigmaValue
+    if sigmaValue == newValue:
+        return
+
+    sigmaValue = newValue
+    print('(sigma)', sigmaValue)
+
+
+"""
+Window Functions
+"""
+
 
 window = Window('Filter', imageWindow.window.image,
                 offset=(round(imageWindow.window.preview.shape[0]*1.8), 0))
 window.addTrackbar('Sigma: ', 0, 5, sigmaOnChange)
-window.addTrackbar('Size: ', 0, 4, noop2)
+window.addTrackbar('Size: ', 0, 4, noopFunc)
 window.show()
