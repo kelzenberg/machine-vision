c9ab6f419189fb08b981200e9c512d22e6fb5624
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-11 01:08:00 +0100

    add update functions for trackbar values

--- U2/TrackbarValues.py
+++ U2/TrackbarValues.py
@@ -7,3 +7,17 @@ class TrackbarValues:
     filter = 0
     sigma = 0
     kernel = 0
+
+    @classmethod
+    def updateFilter(self, value):
+        self.filter = value
+
+    @classmethod
+    def updateSigma(self, value):
+        self.sigma = value
+        self.kernel = 1 # needed for sigma
+
+    @classmethod
+    def updateKernel(self, value):
+        self.kernel = value
+        self.sigma = 0 # needed to not overwrite kernel
