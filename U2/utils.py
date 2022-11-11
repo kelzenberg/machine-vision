07fc7a3b69637e93484c80f83d01d05fb40d1491
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-11 22:39:55 +0100

    add median blur to utils

--- U2/utils.py
+++ U2/utils.py
@@ -8,7 +8,16 @@ Utility functions
 
 
 def runGaussian():
+    multiplier = 10
     global filteredImage
-    filteredImage = cv2.GaussianBlur(grayImage.copy(), (TrackbarValues.kernel*20+1,
-                                     TrackbarValues.kernel*20+1), TrackbarValues.sigma, borderType=cv2.BORDER_REFLECT_101)
+    filteredImage = cv2.GaussianBlur(grayImage.copy(), (TrackbarValues.kernel * multiplier + 1,
+                                     TrackbarValues.kernel * multiplier + 1), TrackbarValues.sigma, borderType=cv2.BORDER_REFLECT_101)
+    return filteredImage
+
+
+def runMedian():
+    multiplier = 6
+    global filteredImage
+    filteredImage = cv2.medianBlur(
+        grayImage.copy(), TrackbarValues.kernel * multiplier + 1)
     return filteredImage
