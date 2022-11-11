e39b09669bd1a0eb6e62c40678eaaae91df1abde
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-11 23:46:33 +0100

    use image update functions in utils

--- U2/utils.py
+++ U2/utils.py
@@ -9,15 +9,13 @@ Utility functions
 
 def runGaussian():
     multiplier = 10
-    global filteredImage
-    filteredImage = cv2.GaussianBlur(Images.gray.copy(), (TrackbarValues.kernel * multiplier + 1,
-                                     TrackbarValues.kernel * multiplier + 1), TrackbarValues.sigma, borderType=cv2.BORDER_REFLECT_101)
-    return filteredImage
+    Images.updateFiltered(cv2.GaussianBlur(Images.gray.copy(), (TrackbarValues.kernel * multiplier + 1,
+                                                                TrackbarValues.kernel * multiplier + 1), TrackbarValues.sigma, borderType=cv2.BORDER_REFLECT_101))
+    return Images.filtered
 
 
 def runMedian():
     multiplier = 6
-    global filteredImage
-    filteredImage = cv2.medianBlur(
-        Images.gray.copy(), TrackbarValues.kernel * multiplier + 1)
-    return filteredImage
+    Images.updateFiltered(cv2.medianBlur(
+        Images.gray.copy(), TrackbarValues.kernel * multiplier + 1))
+    return Images.filtered
