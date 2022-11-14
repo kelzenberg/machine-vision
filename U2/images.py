8bbd12b65c37c20fc70439fb834a6e728fba55d4
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-14 15:52:55 +0100

    add more images to image store

--- U2/images.py
+++ U2/images.py
@@ -16,6 +16,10 @@ class Images:
     gray = cv2.cvtColor(mainImage, cv2.COLOR_RGB2GRAY)
     filtered = gray.copy()
     edges = gray.copy()
+    gradientX = gray.copy()
+    gradientY = gray.copy()
+    gradientXY = gray.copy()
+    binary = gray.copy()
 
     @classmethod
     def updateGray(self, image):
@@ -28,6 +32,22 @@ class Images:
     @classmethod
     def updateEdges(self, image):
         self.edges = image
+
+    @classmethod
+    def updateGradientX(self, image):
+        self.gradientX = image
+
+    @classmethod
+    def updateGradientY(self, image):
+        self.gradientY = image
+
+    @classmethod
+    def updateGradientXY(self, image):
+        self.gradientXY = image
+
+    @classmethod
+    def updateBinary(self, image):
+        self.binary = image
 
 
 print(
