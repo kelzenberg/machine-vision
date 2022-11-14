967e1f0b3315a9fd9a59dfbb0f134a159d1b400a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-14 15:53:32 +0100

    update grad X, Y and XY images

--- U2/utils.py
+++ U2/utils.py
@@ -26,8 +26,11 @@ def runSobel():
     ), TrackbarValues.threshold, 255, cv2.THRESH_BINARY)
     gradientX = cv2.convertScaleAbs(
         cv2.Sobel(threshold, cv2.CV_64F, 1, 0, borderType=cv2.BORDER_REFLECT_101))
+    Images.updateGradientX(gradientX)
     gradientY = cv2.convertScaleAbs(
         cv2.Sobel(threshold, cv2.CV_64F, 0, 1, borderType=cv2.BORDER_REFLECT_101))
+    Images.updateGradientY(gradientY)
     gradient = cv2.addWeighted(gradientX, 0.5, gradientY, 0.5, 0)
+    Images.updateGradientXY(gradient)
     Images.updateEdges(gradient)
     return Images.edges
