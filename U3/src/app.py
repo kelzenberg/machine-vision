834ef23d22ee0ed7559170cbd0564aa39601f970
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-20 13:57:48 +0100

    load all images from images dir

--- U3/src/app.py
+++ U3/src/app.py
@@ -1,16 +1,28 @@
 """
 U3 app
 """
+import os
 import cv2
+from glob import glob
 from GUI.Window import Window
 
-print('(main) Window loaded.')
+"""
+Load Images
+"""
+images = {}
+globPath = os.path.join(os.path.abspath('./images'), '*.png')
+
+for file in sorted(glob(globPath)):
+    image = cv2.imread(file)
+    imageName = file.split('/')[-1]
+    images[imageName] = image
+    print(f'(main) Image loaded: {imageName}')
 
 """
 Main function
 """
 
-mainWindow = Window('Test')
+print(f'(main) {len(images)} Images loaded')
 
 print('(main) Press ESC to exit...')
 while cv2.waitKey(0) != 27:
