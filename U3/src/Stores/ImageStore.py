aec649eed5e4d43c42ad4edfde9848439a89b7b9
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2022-11-20 17:30:30 +0100

    add Image Store

---
+++ U3/src/Stores/ImageStore.py
@@ -0,0 +1,17 @@
+"""
+Image Store
+"""
+from typing import Dict
+from numpy import ndarray
+
+
+class ImageStore:
+    images: Dict[str, ndarray] = {}
+
+    @classmethod
+    def add(self, name: str, image: ndarray):
+        self.images.update({name: image})
+
+    @classmethod
+    def get(self, name: str):
+        return self.images.get(name)
