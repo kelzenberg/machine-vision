fa1047d50b2c9510e3634f5a4f1c30ba44fcb166
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-01-03 16:08:45 +0100

    add range mapping util functions

--- U5/utils.py
+++ U5/utils.py
@@ -2,4 +2,14 @@
 Image Utils
 """
 
-import cv2
+from numpy import arange, interp, ceil
+
+
+def mapValueToRange(value, fromRange, toRange, step):
+    fp = arange(toRange[0], toRange[1], step)
+    xp = arange(fromRange[0], fromRange[1], fromRange[1]/len(fp))
+    return interp(value, xp, fp)
+
+
+def findNearestOddInt(number):
+    return int(ceil(number) // 2 * 2 + 1)
