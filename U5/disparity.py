e49ffff1b5a80e8e07dd8e34c503f6ad2309aa80
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-01-08 17:22:44 +0100

    pick stereo matcher based on trackbar value

--- U5/disparity.py
+++ U5/disparity.py
@@ -7,23 +7,30 @@ from numpy import float32
 from ImageStore import ImageStore
 
 
-def findDisparities(left, right, numDisparities, blockSize, preFilterSize, preFilterCap, textureThreshold, minDisparity):
+def findDisparities(left, right, matcher, numDisparities, blockSize, preFilterSize, preFilterCap, textureThreshold, minDisparity):
     leftName, leftImage = left
     rightName, rightImage = right
 
     print(
-        f'(findDisparities) Find disparities between {leftName} and {rightName}')
+        f'(findDisparities) Find disparities between {leftName} and {rightName} with matcher {"SGBM" if matcher == 1 else "BM"}')
     print(
         f'(findDisparities) With options:\n      disparity: {numDisparities}\n      blockSize: {blockSize}\n      preFilterSize: {preFilterSize}\n      preFilterCap: {preFilterCap}\n      textureThreshold: {textureThreshold}\n      minDisparity: {minDisparity}')
 
-    stereoBM = cv2.StereoBM_create(
-        numDisparities=numDisparities, blockSize=blockSize)
-    stereoBM.setPreFilterSize(preFilterSize)
-    stereoBM.setPreFilterCap(preFilterCap)
-    stereoBM.setTextureThreshold(textureThreshold)
-    stereoBM.setMinDisparity(minDisparity)
+    blockMatcher = None
 
-    computed = stereoBM.compute(leftImage, rightImage)
+    if (matcher == 1):  # use SGBM
+        blockMatcher = cv2.StereoSGBM_create(
+            minDisparity=minDisparity, numDisparities=numDisparities, blockSize=blockSize, preFilterCap=preFilterCap)
+    else:  # use default BM
+        blockMatcher = cv2.StereoBM_create(
+            numDisparities=numDisparities, blockSize=blockSize)
+        blockMatcher.setPreFilterSize(preFilterSize)
+        blockMatcher.setPreFilterCap(preFilterCap)
+        blockMatcher.setTextureThreshold(textureThreshold)
+        blockMatcher.setMinDisparity(minDisparity)
+
+    computed = blockMatcher.compute(leftImage, rightImage)
+
     # normalize to 255 gray values
     computed = (computed.astype(float32)/16.0 - minDisparity) / numDisparities
 
