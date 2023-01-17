ef412808e4e9205b802781727b40cda9318018cc
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-01-17 13:09:42 +0100

    improve video file stats

--- Project/app.py
+++ Project/app.py
@@ -30,7 +30,8 @@ Video Feed
 
 
 def loadVideoFeed():
-    feed = cv2.VideoCapture(0)
+    # feed = cv2.VideoCapture(0)
+    feed = cv2.VideoCapture("./video/test.mp4")
 
     if not feed.isOpened():
         print("(main) Cannot access camera feed.")
@@ -39,7 +40,10 @@ def loadVideoFeed():
     width = int(feed.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(feed.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = int(feed.get(cv2.CAP_PROP_FPS))
-    print(f'(main) Video Feed loaded: {width}x{height} @ {fps}fps')
+    fourcc = int(feed.get(cv2.CAP_PROP_FOURCC))
+    codec = bytes([v & 255 for v in (fourcc, fourcc >> 8,
+                  fourcc >> 16, fourcc >> 24)]).decode() # Source: https://stackoverflow.com/a/71838016
+    print(f'(main) Video Feed loaded: {width}x{height} @ {fps}fps ({codec})')
 
     return feed
 
@@ -84,8 +88,8 @@ print('---> Awaiting input...\n\n')
 
 while True:
     frame = retrieveFrame(feed)
-    detected = detectUpperBody(frame)
-    mainWindow.show('Live Detection Feed', detected)
+    # detected = detectUpperBody(frame)
+    mainWindow.show('Live Detection Feed', frame)
 
     key = cv2.waitKey(1)
     if key == 27:  # key "ESC"
